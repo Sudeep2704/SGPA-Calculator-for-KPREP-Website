74b8d684116f7155f240a2dbd2f4baf7b1b4362f
Sudeep Kumar Manna <Ds.sudeep04@gmail.com>
2025-01-07 11:48:07 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// <<<<<<< Updated upstream
+import React from 'react';
+import Select from './Components/Select'
+export default function App() {
+
+
+  return (
+    <div>
+      <Select />
+    </div>
+  );
+     
+}
